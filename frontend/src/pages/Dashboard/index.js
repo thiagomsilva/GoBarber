b0265d06c6286cb1eef52396219e93620a63575c
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2020-07-16 17:15:59 -0300

    header data, logout aplication and style dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
-function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Magalhães</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Silva</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Thiago M S</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
-
-export default Dashboard;
